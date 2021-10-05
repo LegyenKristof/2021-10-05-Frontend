cef620f984f32812dd374d9f91455dd909950d01
Kristof <you@example.com>
2021-10-05 14:05:30 +0200

    Film osztály hozzáadása

---
+++ main.js
@@ -0,0 +1,12 @@
+class Film{
+    constructor(nev, rendezo, kiadasiEv){
+        this.nev = nev;
+        this.rendezo = rendezo;
+        this.kiadasiEv = kiadasiEv;
+    }
+}
+
+let filmLista = {};
+
+
+
