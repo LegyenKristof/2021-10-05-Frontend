8ecd2531d7f0526bc1e1f8ca55d935b07614e30c
Kristof <you@example.com>
2021-10-05 15:15:01 +0200

    Táblázat létrehozása

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let filmLista = []
+document.getElementById("hozzaAdButton").addEventListener("click", hozzaAd)
+document.getElementById("listazasButton").addEventListener("click", listazas)
 
 class Film{
     constructor(nev, rendezo, kiadasiEv){
@@ -7,8 +9,6 @@ class Film{
         this.kiadasiEv = kiadasiEv
     }
 }
-
-document.getElementById("hozzaAdButton").addEventListener("click", hozzaAd)
 
 function hozzaAd(){
     let nev = document.getElementById("nevInput").value
@@ -21,4 +21,16 @@ function hozzaAd(){
     document.getElementById("nevInput").value = ""
     document.getElementById("rendezoInput").value = ""
     document.getElementById("kiadasiEvInput").value = ""
+}
+
+function listazas(){
+    let table = document.getElementById("table");
+    table.innerHTML = "<tr><th>Név</th><th>Rendező</th><th>Kiadási év</th></tr>"
+
+    for (let i = 0; i < filmLista.length; i++){
+        let tr = table.insertRow()
+        tr.insertCell(0).innerHTML = filmLista[i].nev
+        tr.insertCell(1).innerHTML = filmLista[i].rendezo
+        tr.insertCell(2).innerHTML = filmLista[i].kiadasiEv
+    }
 }
