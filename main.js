fd342c8ccea79d6ee92a627b097ad3bde2b7fee4
Kristof <you@example.com>
2021-10-05 14:32:57 +0200

    Inputok hozzáadása és kezelése

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+let filmLista = []
+
 class Film{
     constructor(nev, rendezo, kiadasiEv){
-        this.nev = nev;
-        this.rendezo = rendezo;
-        this.kiadasiEv = kiadasiEv;
+        this.nev = nev
+        this.rendezo = rendezo
+        this.kiadasiEv = kiadasiEv
     }
 }
 
-let filmLista = {};
+document.getElementById("hozzaAdButton").addEventListener("click", hozzaAd)
 
+function hozzaAd(){
+    let nev = document.getElementById("nevInput").value
+    let rendezo = document.getElementById("rendezoInput").value
+    let kiadasiEv = document.getElementById("kiadasiEvInput").value
+    
+    let ujFilm = new Film(nev, rendezo, kiadasiEv)
+    filmLista.push(ujFilm)
 
-
+    document.getElementById("nevInput").value = ""
+    document.getElementById("rendezoInput").value = ""
+    document.getElementById("kiadasiEvInput").value = ""
+}
